b0fef884f621effc6eb0b98642e12cbc3c2e0489
pinenutss <627605616@qq.com>
2018-06-27 07:26:44 +0800

    初始化项目

---
+++ src/page/home/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header
+  display flex
+  line-height 0.86rem
+  background #00bcd4
+  color #fff
+  .header-left
+    width 0.64rem
+    float left
+  .header-input
+    flex 1
+    display flex
+    height 0.64rem
+    line-height .64rem
+    margin-top 0.12rem
+    margin-left 0.2rem
+    background #ffffff
+    border-radius 0.1rem
+    color #ccc
+    padding-left .1rem
+  .header-right
+    width 1.24rem
+    float right
+    text-align center
+</style>
