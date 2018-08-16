a1df7dcb9b731619e562a32514b497b47c19454b
pinenutss <627605616@qq.com>
2018-08-16 10:54:03 +0800

    ss

--- src/page/home/components/Header.vue
+++ src/page/home/components/Header.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">城市
+      <span class="iconfont">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -21,17 +27,20 @@ export default {
   .header-left
     width 0.64rem
     float left
+    .back-icon
+      text-align center
+      font-size 0.4rem
   .header-input
     flex 1
     display flex
     height 0.64rem
-    line-height .64rem
+    line-height 0.64rem
     margin-top 0.12rem
     margin-left 0.2rem
     background #ffffff
     border-radius 0.1rem
     color #ccc
-    padding-left .1rem
+    padding-left 0.1rem
   .header-right
     width 1.24rem
     float right
